36c0f0660de3cd9cc831942c7b245a09b3a1ea2e
0x <42121860+x00x0x00x@users.noreply.github.com>
2020-12-26 01:46:18 +0100

    loading overlay

--- web1/main.js
+++ web1/main.js
@@ -1,35 +1,14 @@
 
-// if (!String.prototype.format) {
-//   String.prototype.format = function() {
-//     var args = arguments;
-//     return this.replace(/{(\d+)}/g, function(match, number) { 
-//       return typeof args[number] != 'undefined'
-//         ? args[number]
-//         : match
-//       ;
-//     });
-//   };
-// }
+function hideDiv(div_id){
+	document.getElementById(div_id).style.display = 'none';
+}
 
-// async function fetchHtmlAsText(url) {
-//     return await (await fetch(url)).text();
-// }
-
-// async function loadToDiv(div_id,file_path) {
-//     const contentDiv = document.getElementById(div_id);
-//     contentDiv.innerHTML = await fetchHtmlAsText(file_path);
-// }
-
-// function loadToDiv(div_id, file_path){
-	// $("#{0}".format(div_id)).load("{0}".format(file_path));
-	// $("#topDiv").load("inc/header.html");
-	// document.getElementById(div_id).innerHTML='<object type="text/html" data="{0}" ></object>'.format(file_path);
-// }
-// loadToDiv('topDiv', 'inc/header.html');
-
+// aangezien we gebruik maken van een header en footer die op beide pagina's hetzelfde zijn gaan we die op alle pagina's inladen
 $("#topDiv").load("inc/header.html");
 
 $(document).ready(function(){
 	$('.header').height($(window).height());
 
+	// nadat alles ingeladen is (de jquery die de header/footer inlaad), verbergen we de loading overlay en geven we de content weer
+	hideDiv('loadingOverlay');
 })
