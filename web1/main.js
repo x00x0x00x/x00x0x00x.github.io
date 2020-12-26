179351b4d75413d8711b438df334eed88ede9028
0x <42121860+x00x0x00x@users.noreply.github.com>
2020-12-26 02:08:13 +0100

    push to head test

--- web1/main.js
+++ web1/main.js
@@ -5,6 +5,7 @@ function hideDiv(div_id){
 
 // aangezien we gebruik maken van een header en footer die op beide pagina's hetzelfde zijn gaan we die op alle pagina's inladen
 $("#topDiv").load("inc/header.html");
+$('#pushImports').append($('').load('ajax/test.html'));
 
 $(document).ready(function(){
 	$('.header').height($(window).height());
